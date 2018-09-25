451cbd0f7b5ae87b0b9a7ff1ddd2d6307d55211e
thamshid <thamshid@qburst.com>
2018-09-25 14:02:10 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,24 @@
+# Word matching
+
+## Getting Started
+
+
+
+### Install requirements
+```
+pip install -r requirements.txt
+```
+
+
+### Run Server
+```
+python server.py
+```
+
+### API Details
+API: search/
+Query parameter: key
+example:
+```
+http://127.0.0.1:5000/search?key=Now is
+```
---
+++ server.py
@@ -0,0 +1,44 @@
+import re
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+def get_sentence(lines, line_no, start, end, key):
+    starting_sentence = "".join(lines[:line_no - 1]) + lines[line_no][:start]
+
+    ending_sentence = lines[line_no][end:] + "".join(lines[line_no + 1:])
+    starting_sentence = starting_sentence[starting_sentence.rfind('.') + 1:]
+    ending_sentence = ending_sentence[:ending_sentence.find('.')]
+    return (starting_sentence + key + ending_sentence).replace("\n", "")
+
+
+def get_occurance(key):
+    result = []
+    with open('king-i.txt') as f:
+        lines = f.readlines()
+    for i in range(len(lines)):
+        matches = list(re.finditer(key, lines[i]))
+        for match in matches:
+            in_sentence = get_sentence(lines, i, match.start(), match.end(), key)
+            result.append(
+                {"start:": match.start() + 1, "end": match.end() + 1, "line": i + 1, "in_sentence": in_sentence})
+
+    return result
+
+
+@app.route("/search")
+def search():
+    key = request.args.get('key')
+    occurrences = get_occurance(key)
+    data = {
+        "query_text": key,
+        "number_of_occurrences": len(occurrences),
+        "occurrences": occurrences
+    }
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run()
